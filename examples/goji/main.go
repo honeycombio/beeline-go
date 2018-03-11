a616bacfa188f92f02d2ab20420a1d9b183da897
Ben Hartshorne <ben@honeycomb.io>
2018-03-10 21:50:04 -0800

    added goji

--- examples/goji/main.go
+++ examples/goji/main.go
@@ -13,26 +13,27 @@ const writekey = "cf80cea35c40752b299755ad23d2082e"
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	honeycomb.AddField(r.Context(), "custom", "Wheee")
-	name := pat.Param(r, "name") // pat is automatically added to the event
-	fmt.Fprintf(w, "Hello, %s!", name)
+	name := pat.Param(r, "name")         // pat is automatically added to the event
+	relation := pat.Param(r, "relation") // pat is automatically added to the event
+	fmt.Fprintf(w, "Hello, my %s %s!", relation, name)
 }
 
 func bye(w http.ResponseWriter, r *http.Request) {
 	honeycomb.AddField(r.Context(), "custom", "Wheee")
 	name := pat.Param(r, "name") // pat is automatically added to the event
-	fmt.Fprintf(w, "Hello, %s!", name)
+	fmt.Fprintf(w, "goodbye, %s!", name)
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
 	honeycomb.AddField(r.Context(), "custom", "Wheee")
 	name := pat.Param(r, "name") // pat is automatically added to the event
-	fmt.Fprintf(w, "Hello, %s!", name)
+	fmt.Fprintf(w, "regging, %s!", name)
 }
 
 func deregister(w http.ResponseWriter, r *http.Request) {
 	honeycomb.AddField(r.Context(), "custom", "Wheee")
 	name := pat.Param(r, "name") // pat is automatically added to the event
-	fmt.Fprintf(w, "Hello, %s!", name)
+	fmt.Fprintf(w, "dregging, %s!", name)
 }
 
 func main() {
@@ -40,9 +41,17 @@ func main() {
 	root := goji.NewMux()
 	greetings := goji.SubMux()
 	registrations := goji.SubMux()
-	greetings.HandleFunc(pat.Get("/hello/:name"), hello)
+	root.Handle(pat.New("/greet/*"), greetings)
+	root.Handle(pat.New("/reg/*"), registrations)
+	greetings.HandleFunc(pat.Get("/hello/:name/:relation"), hello)
+	greetings.HandleFunc(pat.Get("/bye/:name"), bye)
+	registrations.HandleFunc(pat.Get("/register/:name"), register)
+	registrations.HandleFunc(pat.Get("/deregister/:name"), deregister)
 
-	http.ListenAndServe("localhost:8000", hi.InstrumentMuxHandler(greetings))
+	greetings.Use(hi.InstrumentGojiMiddleware)
+	registrations.Use(hi.InstrumentGojiMiddleware)
+	http.ListenAndServe("localhost:8000", root)
+	// http.ListenAndServe("localhost:8000", root)
 }
 
 // root := NewMux()
--- honeycomb.go
+++ honeycomb.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	libhoney "github.com/honeycombio/libhoney-go"
+	"goji.io/middleware"
+	"goji.io/pat"
 )
 
 const honeyEventContextKey = "honeycombEventContextKey"
@@ -43,6 +45,9 @@ func NewInstrumenter(wk string) *Hny {
 
 func AddField(ctx context.Context, key string, val interface{}) {
 	ev := existingEventFromContext(ctx)
+	if ev == nil {
+		return
+	}
 	ev.AddField(key, val)
 }
 
@@ -124,6 +129,63 @@ func (h *Hny) InstrumentMuxHandler(mux *http.ServeMux) http.Handler {
 		hType := reflect.TypeOf(handler)
 		ev.AddField("handlerType", hType.String())
 		ev.AddField("handler_name", runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name())
+		handler.ServeHTTP(wrappedWriter, r)
+		if wrappedWriter.status == 0 {
+			wrappedWriter.status = 200
+		}
+		ev.AddField("response.status_code", wrappedWriter.status)
+		ev.AddField("duration_ms", float64(time.Since(start))/float64(time.Millisecond))
+		ev.Send()
+	}
+	return http.HandlerFunc(wrappedHandler)
+}
+
+func (h *Hny) InstrumentGojiMiddleware(handler http.Handler) http.Handler {
+	wrappedHandler := func(w http.ResponseWriter, r *http.Request) {
+		// TODO find out if we're a sub-handler and don't stomp the parent
+		// event, or at least get parent/child IDs and intentionally send a
+		// subevent or something
+		start := time.Now()
+		ev := libhoney.NewEvent()
+		// put the event on the context for everybody downsteam to use
+		r = r.WithContext(context.WithValue(r.Context(), honeyEventContextKey, ev))
+		ctx := r.Context()
+		// add some common fields from the request to our event
+		addRequestProps(r, ev)
+		// replace the writer with our wrapper to catch the status code
+		wrappedWriter := &hnyResponseWriter{ResponseWriter: w}
+		// get bits about the handler
+		handler := middleware.Handler(ctx)
+		if handler == nil {
+			ev.AddField("handler_name", "http.NotFound")
+			handler = http.NotFoundHandler()
+		} else {
+			hType := reflect.TypeOf(handler)
+			ev.AddField("handlerType", hType.String())
+			ev.AddField("handler_name", runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name())
+		}
+		// find any matched patterns
+		pm := middleware.Pattern(ctx)
+		if pm == nil {
+			ev.AddField("goji_pat", "nil")
+		} else {
+			// TODO put a regex on `p.String()` to pull out any `:foo` and then
+			// use those instead of trying to pull them out of the pattern some
+			// other way
+			if p, ok := pm.(*pat.Pattern); ok {
+				ev.AddField("pat", "is a pat.Pattern")
+				ev.AddField("goji_pat", p.String())
+				ev.AddField("goji_methods", p.HTTPMethods())
+				ev.AddField("goji_path_prefix", p.PathPrefix())
+				patvar := strings.TrimPrefix(p.String(), p.PathPrefix()+":")
+				ev.AddField("patvar", patvar)
+				// ev.AddField("patval", pat.Param(r, patvar))
+			} else {
+				ev.AddField("pat", "NOT pat.Pattern")
+
+			}
+		}
+		// TODO get all the parameters and their values
 		handler.ServeHTTP(wrappedWriter, r)
 		if wrappedWriter.status == 0 {
 			wrappedWriter.status = 200
