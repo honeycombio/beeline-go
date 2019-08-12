20c85e630f46bc039b425176564456e05106d638
Alex Gaynor <alex.gaynor@gmail.com>
2019-08-12 09:33:15 -0400

    Added Close() to Stmt in sqlx -- this method is on the underlying sqlx type, but was not exposed in the wrapper

--- wrappers/hnysqlx/sqlx.go
+++ wrappers/hnysqlx/sqlx.go
@@ -1320,6 +1320,15 @@ func (s *Stmt) Unsafe() *Stmt {
 	return s
 }
 
+func (s *Stmt) Close() error {
+	var err error
+	_, sender := common.BuildDBEvent(s.Builder, "")
+	defer sender(err)
+
+	err = s.wstmt.Close()
+	return err
+}
+
 type Tx struct {
 	wtx     *sqlx.Tx
 	Builder *libhoney.Builder
